891ee4ef2db472f6bf2fdc037b4c041ee380443f
pandabear100 <nelsonysun@gmail.com>
2022-04-03 17:44:11 -0700

    Everything works except actually drawing the graph

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,36 @@
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args){
-        JFrame frame = new JFrame("Grip Strength");
-        ProgramState ps = new ProgramState();
-        ProgramGUI gui = new ProgramGUI(ps);
 
-        BrainFlowMain dataUpdater = new BrainFlowMain(ps);
-        dataUpdater.start();
+    private static JFrame frame;
+    private static ProgramState ps;
+    private static ProgramGUI gui;
+
+    public static void main(String[] args){
+        frame = new JFrame("Grip Strength");
+        ps = new ProgramState();
+        gui = new ProgramGUI(ps);
+
+        //BrainFlowMain dataUpdater = new BrainFlowMain(ps);
+        //dataUpdater.start();
 
         frame.setContentPane(gui.getRootPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         gui.setStatusMessage("<html>Grip Strength Diagnostic. Apply electrodes as depicted.<br/>When complete, press start.</html>");
-        while(!ps.hasScanStarted()){
+    }
 
+    public static void startDetection(){
+        gui.setStatusMessage("<html>Relax your lower arm on a horizontal surface. <br/>Slowly grip and release.</html>");
+        ps.setCurrentReading(new CytonData(new double[]{2, 7, 9, 4}, 3));
+        while(ps.hasScanStarted()){
+            gui.render();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("Press detected"); //remove when done
-        gui.setStatusMessage("Relax your lower arm on a horizontal surface. Slowly grip and release.");
-
     }
 }
--- src/ProgramGUI.java
+++ src/ProgramGUI.java
@@ -3,10 +3,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Line2D;
 import java.io.File;
 import java.io.IOException;
 
-public class ProgramGUI {
+public class ProgramGUI extends Thread{
     private JPanel rootPanel;
     private JLabel status;
     private JButton pressButton;
@@ -14,7 +15,7 @@ public class ProgramGUI {
     private JPanel graph;
 
     private ProgramState ps;
-    boolean status_on = false;
+    //boolean status_on = false;
 
     private final String soundName = "sfx.wav";
     private AudioInputStream audioIS;
@@ -22,27 +23,12 @@ public class ProgramGUI {
 
     public ProgramGUI(ProgramState programState) {
         ps = programState;
+        graph.setOpaque(true);
         pressButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                status_on = !status_on;
-                if(status_on){
-                    setStatusMessage("Grip detected");
-                }
-                else{
-                    if(c != null) {
-                        c.close();
-                    }
-                    try {
-                        audioIS = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
-                        c = AudioSystem.getClip();
-                        c.open(audioIS);
-                    } catch (LineUnavailableException | IOException | UnsupportedAudioFileException ex) {
-                        ex.printStackTrace();
-                    }
-                    c.start();
-                    setStatusMessage("Grip not detected");
-                }
+                ps.hasScanStarted();
+                Main.startDetection();
             }
         });
     }
@@ -63,7 +49,49 @@ public class ProgramGUI {
         pressButton.setEnabled(enable);
     }
 
+    public void render(){
+        graph.paintComponents(graph.getGraphics());
+    }
+
     private void createUIComponents() {
-        graph = new JGraph();
+        graph = new JPanel() {
+            public void paintComponents(Graphics g) {
+                super.paintComponent(g);
+                double[] data = ps.getCurrentReading().getData();
+                int margin = 5;
+                Graphics2D g1=(Graphics2D)g;
+                g1.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+                int width = this.getWidth();
+                int height = this.getHeight();
+                double minY = minValue();
+                double maxY = maxValue();
+                double deltaX = ((double) width - 2 * margin) / data.length;
+                double deltaY = ((double) height - 2 * margin) / (maxY - minY);
+                g1.setPaint(Color.blue);
+                for(int i = 1; i < data.length; i++){
+                    double x0 = margin + deltaX * (i - 1);
+                    double y0 = margin + deltaY * (data[i - 1] - minY);
+                    double x1 = margin + deltaX * i;
+                    double y1 = margin + deltaY * (data[i] - minY);
+                    g1.draw(new Line2D.Double(x0, y0, x1, y1));
+                }
+            }
+        };
+    }
+
+    private double maxValue(){
+        double max = Double.MIN_VALUE;
+        for(double d : ps.getCurrentReading().getData()){
+            max = Math.max(d, max);
+        }
+        return max;
+    }
+
+    private double minValue(){
+        double min = Double.MAX_VALUE;
+        for(double d : ps.getCurrentReading().getData()){
+            min = Math.min(d, min);
+        }
+        return min;
     }
 }
